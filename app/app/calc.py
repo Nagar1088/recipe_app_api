cb1e40a1d8986e28f43e952a256f1fc96e496d24
nagar1088 <sachinnagar094@gmail.com>
2024-05-16 12:25:17 +0530

    Added psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+""""
+calculator function
+"""
+
+def add(x,y):
+    """ Add x and y and return result."""
+    return x+y
+
+def substract(x, y):
+    """substract x and y and return result."""
+    return y-x
